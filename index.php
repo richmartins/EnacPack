4763b2a5715279efa40a68215dcec6dad1fc23d8
richmartins <rvmartin@it3pc1.epfl.ch>
2017-06-02 16:54:30 +0200

    have to make json validated

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 
-  require_once('templates/template.html');
+  $html = require_once('templates/template.html');
 
 
-
-
-
+  $data  = file_get_contents('./asset/shellCom.json', FILE_USE_INCLUDE_PATH);
+  $json = json_decode($data, TRUE);
+  foreach ($json->{'shell'} as $key => $value) {
+    if(!is_array($value)){
+      echo $key . '=>' . $value . '<br/>';
+    } else {
+      foreach ($value as $key => $val){
+        echo $key . '=>' . $value . '<br />';
+      }
+    }
+  }
 
 ?>
