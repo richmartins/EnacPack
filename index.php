e50258a5e7b405652e04f2714d53fae28053baf3
Richard Tenorio <richard.martinstenorio@epfl.ch>
2017-06-21 08:33:42 +0200

    json done

--- index.php
+++ index.php
@@ -6,15 +6,9 @@
   $data  = file_get_contents('./asset/shellCom.json', FILE_USE_INCLUDE_PATH);
 
   $json = json_decode($data);
-  echo $json;
-  foreach ($json->{'shell'} as $key => $value) {
-    if(!is_array($value)){
-      echo $key . '=>' . $value . '<br/>';
-    } else {
-      foreach ($value as $key => $val){
-        echo $key . '=>' . $value . '<br />';
-      }
-    }
+
+  foreach ($json->command as $shell) {
+    
   }
 
 ?>
