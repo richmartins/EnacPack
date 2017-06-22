0507a132a7abdd3b9d9d00fc193dfc649be25849
rvmartin <rvmartin@it3pc1.epfl.ch>
2017-06-22 16:40:08 +0200

    phping

--- index.php
+++ index.php
@@ -2,18 +2,31 @@
 
   $html = require_once('templates/template.html');
 
-
   $data  = file_get_contents('./asset/shellCom.json', FILE_USE_INCLUDE_PATH);
 
   $json = json_decode($data);
 
+  //gotta prepare the loop that will generate the cards with the Radio Button
+  foreach ($json->command as $commands){
+    $id = $commands->id;
+    $name = $commands->name;
+    echo $id;
+    echo $name;
+    $img = '<img alt=" ' . $name . ' " src="./asset/' . $name . '.png"  />';
+    $lab = '<label>' . $name . '</label><br /> ';
+    $input = '<input type="radio" name=" ' . $name . ' " id=" ' . $id . '" />';
 
-  foreach ($json->command as $shellcom) {
-    if(strpos($shellcom->shell, "\n") !== FALSE){
+    //add to the html but how ?
+  }
+
+  //where i will parse the shells command
+
+  foreach ($json->command as $commands) {
+  if(str_replace($commands->shell, "\n", '<br />') /*!== FALSE*/){
       echo '<br />';
     }
 
-    echo $shellcom->shell . "<br />";
+    //echo $shellcom->shell . "<br />";
 
   }
 
