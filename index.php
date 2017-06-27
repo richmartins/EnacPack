7694c6b934597de04aae9689c58646e6ec02d126
Richard Tenorio <richard.martinstenorio@epfl.ch>
2017-06-27 21:59:13 +0200

    phpin 2

--- index.php
+++ index.php
@@ -1,33 +1,58 @@
 <?php
 
-  $html = require_once('templates/template.html');
+  libxml_use_internal_errors(true);
+  //loading html
+  $html = file_get_contents('templates/template.html');
 
-  $data  = file_get_contents('./asset/shellCom.json', FILE_USE_INCLUDE_PATH);
+  //instancing object that will make me point in the DOM
+  $doc = new DOMDocument;
+  @$doc->loadHTML($html);
 
+  $classname = 'flex-items';
+  //object that point class CSS
+  $xpath = new DOMXPath($doc);
+
+  $nodes = $finder->query("//*[contains(@class, '$classname')]");
+
+  //loading database(json)
+  $data  = file_get_contents('./asset/shellCom.json');
   $json = json_decode($data);
 
-  //gotta prepare the loop that will generate the cards with the Radio Button
-  foreach ($json->command as $commands){
-    $id = $commands->id;
-    $name = $commands->name;
-    echo $id;
-    echo $name;
-    $img = '<img alt=" ' . $name . ' " src="./asset/' . $name . '.png"  />';
-    $lab = '<label>' . $name . '</label><br /> ';
-    $input = '<input type="radio" name=" ' . $name . ' " id=" ' . $id . '" />';
+  //selecting the things you know
+  //$card = $xpath->getElementsByClassName($expression);
 
-    //add to the html but how ?
+  //generate the cards
+  //cardF($json, $card);
+
+
+  //function that generate the cards
+  function cardF($json, $card){
+    foreach ($json->command as $commands){
+
+      $id = $commands->id;
+      $name = $commands->name;
+
+      $img = ' "<div class="card"><img alt=" ' . $name . ' " src="./asset/' . $name . '.png"  />';
+      $lab = '<label>' . $name . '</label><br /> ';
+      $input = '<input type="radio" name=" ' . $name . ' " id=" ' . $id . '" /></div>';
+      //add to the html but how ?
+      $card .= $img;
+      $card .= $lab;
+      $card .= $input;
+    }
   }
 
-  //where i will parse the shells command
 
-  foreach ($json->command as $commands) {
-  if(str_replace($commands->shell, "\n", '<br />') /*!== FALSE*/){
-      echo '<br />';
+  function parse(){
+    //where i will parse the shells command
+    foreach ($json->command as $commands) {
+    if(str_replace($commands->shell, "\n", '<br />') /*!== FALSE*/){
+        echo '<br />';
+      }
+
+      //echo $shellcom->shell . "<br />";
+
     }
-
-    //echo $shellcom->shell . "<br />";
-
   }
 
 ?>
