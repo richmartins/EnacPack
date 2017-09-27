3f5b68bbbd9d58f2dba653e01382fc46033fbf08
Richard Tenorio <richard.martinstenorio@epfl.ch>
2017-09-27 16:44:45 +0200

    card are WORKING

--- index.php
+++ index.php
@@ -1,62 +1,66 @@
 <?php
+  function cardF($json){
+      foreach ($json->command as $commands){
 
-  //loading database(json)
-  $data  = file_get_contents('./asset/shellCom.json');
-  $json = json_decode($data);
+        $id = $commands->id;
+        $name = $commands->name;
 
-
-function cardF($json, $card){
-    foreach ($json->command as $commands){
-
-      $id = $commands->id;
-      $name = $commands->name;
-
-      $img = ' "<div class="card"><img alt=" ' . $name . ' " src="./asset/' . $name . '.png"  />';
-      $lab = '<label>' . $name . '</label><br /> ';
-      $input = '<input type="radio" name=" ' . $name . ' " id=" ' . $id . '" /></div>';
-      //add to the html
-      $card .= $img;
-      $card .= $lab;
-      $card .= $input;
-    }
-  }
-
-
-  function parse(){
-    //where i will parse the shells command
-    foreach ($json->command as $commands) {
-    if(str_replace($commands->shell, "\n", '<br />') /*!== FALSE*/){
-        echo '<br />';
+        $img = ' "<div class="card"><img alt=" ' . $name . ' " src="./asset/' . $name . '.png"  />';
+        $lab = '<label>' . $name . '</label><br /> ';
+        $input = '<input type="radio" name=" ' . $name . ' " id=" ' . $id . '" /></div>';
+        //add to the html
       }
-
-      //echo $shellcom->shell . "<br />";
-
-    }
   }
 ?>
 <html>
   <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <meta charset="utf-8" />
     <title>Accueil</title>
     <link rel="stylesheet" type="text/css" href="styles/style.css"/>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" ></script>
   </head>
   <body>
-    <div id="container">
-      <div class="header">
-        <div class="headItm"><img  alt="EnacLogo" src="images/LOGO-enac-it3.png" width="173px" height="169px"/></div>
-        <div class="headItm"><h1>EnacPack</h1></div>
-        <div class="headItm"><img alt="EpflLogo" src="images/Logo_EPFL.png" width="173px" height="169px" /></div>
+      <div class="headerImg">
+        <div class="headItem"><img  alt="EnacLogo" src="images/LOGO-enac-it3.png" width="193px" height="189px"/></div>
+        <div class="headItem"><img alt="EpflLogo" src="images/Logo_EPFL.png" width="173px" height="169px" /></div>
       </div>
-      <div class="description">
-        <p>EnacPack is a API for the installation of basic software.</p>
-      </div><hr />
-      <div class="flex-item">
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-3">EnacPack</h1>
+          <p class="lead">EnacPack is a API for the installation of basic software.</p>
+        </div>
+      </div>
+      <div class="container">
+      <hr />
+      <div class="card-deck" style="display: flex;">
+          <?php
+            //loading database(json)
+            $data  = file_get_contents('./asset/shellCom.json');
+            $json = json_decode($data);
 
-      </div>
-      <hr/><div>
+            foreach ($json->command as $commands) {
+
+              $id = ($commands->id);
+              $name = ($commands->name);
+
+              echo '<div class="card" style="width 300px">';
+              echo '<img class="card-img-top" alt="'. $name . '" src="./images/LogoAPP/' . $name . '.png" height="60px" width="60px"/>';
+              echo '<div class="card-body">';
+              echo '<p class="card-title">' . $name . '</p>';
+              echo '<input class="card-text" type="checkbox" name="' . $name . '" id="' . $id .'" aria-label="Radio button for following text input">';
+              echo '</div></div>';
+            }?>
+    </div>
+    <button type="button" class="btn btn-secondary">Install</button>
+  </div>
+      <hr /><div id="footer">
         <p>Richard©</p>
       </div>
     </div>
     <script type="text/javascript" href="scripts/script.js"></script>
+  <!--  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script> -->
   </body>
 </html>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,3 @@
 $(document).ready(function (){
-  $.ajax({
-    dataType: "json".
-    url: "/asset/shellCom.json",
-    data: "data",
-    success:
-  })
 
-  $.getJSON("../asset/shellCom.json", function( data ) {
-    console.log(data);
-    var items = [];
-    $.each( data, function(name, val) {
-      items.push( "<div  id='" + name + "'>" + val "</div>");
-    });
-    $("</div>", {
-      "class": "my-new-card",
-      html: items.join("")
-    }).appendTo("body");
-  })
 }
