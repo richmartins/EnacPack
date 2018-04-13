ef21c1f6e4cde7cbe3f8c797d19a9a23c806c23b
Richard Tenorio <richard.martinstenorio@epfl.ch>
2018-04-13 14:55:30 +0200

    apply button almost perfect 2.0

--- api.php
+++ api.php
@@ -1,29 +1,32 @@
 <?php
-require("init.php");
+  require_once 'init.php';
 
-if ($_POST["method"]=="applyButton"){
-  handleApply($_POST);
-}
+  if ($_POST["method"]=="applyButton"){
 
-function handleApply($myPost) {
-  $json_file = file_get_contents('assets/shellCom.json');
-  $json_dec = json_decode($json_file, true);
+    handleApply($_POST, $shellJSON);
+  }
 
-  $new_id = $myPost['id'];
-  $new_shell = $myPost['shell'];
+  function handleApply($myPost, $json) {
 
-  foreach ($json_dec['command'] as $k => $v) {
-    if($v['id'] == $myPost['id']){
-      $v['id'] = $new_id;
-      $v['shell'] = $new_shell;
+    $json_dec = $json->getJSONdata();
+    $new_id = $myPost['id'];
+    $new_shell = $myPost['shell'];
+
+    foreach ($json_dec['command'] as $k => $v) {
+      if($v['id'] == $myPost['id']){
+        $v['id'] = $new_id;
+        $v['shell'] = $new_shell;
+      }
     }
+
+    foreach(array_keys($json_dec) as $v){
+      $v = iconv('UTF-8','ISO-8859-9', $v);
+    }
+    $enc = json_encode($json_dec);
+
+    file_put_contents('assets/shellCom.json', $enc);
+
+    error_log($json_dec['command'][1]['shell']);
+
+
   }
-
-  foreach(array_keys($json_dec) as $v){
-    $v = iconv('UTF-8','ISO-8859-9', $v);
-  }
-
-  $enc = json_encode($json_dec);
-
-  file_put_contents('assets/shellCom.json', $enc);
-}
--- test.php
+++ test.php
@@ -1,22 +1,23 @@
 <?php
   require_once 'init.php';
 
-  $json_file = file_get_contents('assets/shellCom.json');
-  $json_dec = json_decode($json_file, true);
-
-  $new_content = 'he';
-
-  $i = 1;
-
-  function save_content($json_dec, $index, $new_content){
-    $json_dec['command'][$i]['shell'] = $new_content;
+  $json_dec = $shellJSON->getJSONdata();
 
 
-    foreach(array_keys($json_dec) as $v){
-      $v = iconv('UTF-8','ISO-8859-9', $v);
+  $new_content = 'omggggggg work please';
+
+  $id = 2;
+
+  foreach ($json_dec['command'] as $key => $value) {
+    if($value['id'] == $id){
+        $value['shell'] = $new_content;
     }
-
-    $enc = json_encode($json_dec);
-
-    file_put_contents('assets/shellCom0.json', $enc);
   }
+
+  foreach(array_keys($json_dec) as $v){
+    $v = iconv('UTF-8','ISO-8859-9', $v);
+  }
+
+  $enc = json_encode($json_dec);
+
+  file_put_contents('assets/shellCom.json', $enc);
