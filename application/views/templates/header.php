07a9e1442c9de6d87ce95f9be7a168d941ba3d53
richmartins <richard.martinstenorio@gmail.com>
2020-02-06 16:15:55 +0100

    [WIP] first version of favico

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,6 +4,10 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <link rel="shortcut icon" href="<?=base_url()?>favicon.ico" type="image/x-icon">
+        <link rel="icon" href="<?=base_url()?>favicon.ico" type="image/x-icon">
+        
         <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet">         
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="<?= base_url() ?>public/css/main.css"/>
@@ -21,7 +25,10 @@
     </div>
     <div id="header-container">
         <div id="header-title">
-            <h4><a href="<?= base_url() ?>">ENACPACK</a></h4>
+            <a href="<?= base_url() ?>">
+                <img src="<?= base_url(); ?>public/enacpacklogo.png" height="85px"/>
+                <span>ENACPACK</span>
+            </a>
             <span><?= $description ?></span>
         </div>
     </div>
